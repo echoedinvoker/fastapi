0f8731d5fa40123312beabeb497d2520605768d9
echoedinvoker <echoedinvoker@gmail.com>
2023-03-16 14:42:54 +0800

    qq

---
+++ 3-get-method/12-predefined-value/memo.md
@@ -0,0 +1,11 @@
+## **Build Custom Type with Enum constructor**
+
+> Sometimes we have to restrict the value of the path parameter to a certain range, so we can use the custom type.
+
+![Alt custom type with Enum](pic/17.jpg)
+
+## **TEST**
+
+![Alt test ](pic/18.jpg)
+
+![Alt test error](pic/19.jpg)
--- helloworld/main.py
+++ helloworld/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -10,13 +11,19 @@ def index():
 def get_all_blog():
     return {'message': 'All blogs!'}
 
+class BlogType(str, Enum):
+    short = 'short'
+    long = 'long'
+    howto = 'howto'
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+    return {'message': f'Blog type is {type}'}
+
+
 @app.get('/blog/{id}')
 def get_blog(id: int):
     idx = id + 1
     return {'message': f'Blog with id {idx}'}
-
-# @app.get('/blog/all')
-# def get_all_blog():
-#     return {'message': 'All blogs!'}
 
 
