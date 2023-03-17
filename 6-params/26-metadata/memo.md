878286a81e96b520b28504fac712040214d4b5b0
echoedinvoker <echoedinvoker@gmail.com>
2023-03-17 08:45:28 +0800

    qq

---
+++ 6-params/26-metadata/memo.md
@@ -0,0 +1,21 @@
+## **Query**
+
+> fastapi provides three objects Query, Path, Body to write metadata for each of the three parameters.:w
+
+### _summary & description_
+
+![Alt Query](pic/01.jpg)
+
+![Alt swagger](pic/02.jpg)
+
+### _alias_
+
+![Alt alias](pic/03.jpg)
+
+![Alt swagger](pic/04.jpg)
+
+### _deprecated_
+
+![Alt deprecated](pic/05.jpg)
+
+![Alt swagger](pic/06.jpg)
--- helloworld/router/blog_post.py
+++ helloworld/router/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
 from typing import Optional
 
@@ -13,6 +13,25 @@ class BlogModel(BaseModel):
     pages: int
     published: Optional[bool]
 
-@router.post('/new')
-def create_blog(blog: BlogModel):
-    return {'data': blog}
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        'id': id,
+        'version': version,
+        'data': blog
+        }
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel, id: int, 
+                   comment_id: int = Query(None,
+                        title="Id of the comment",
+                        description="Some description for commend_id",
+                        alias='commetId',
+                        deprecated=True
+                   )
+                ):
+    return {
+        'id': id,
+        'commend_id': comment_id,
+        'data': blog
+        }
